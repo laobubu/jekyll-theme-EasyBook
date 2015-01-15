e4708d1660afc42f68795caf0be62d2dd1c4bfe6
laobubu <laobubu@gmail.com>
2015-01-15 09:48:17 +0000

    TOC Width

--- js/easybook.js
+++ js/easybook.js
@@ -72,7 +72,7 @@ function TOC(toc,content) {
         if (d != maxHeightTOC) {
             maxHeightTOC = d;
             if (d) {
-                uls[0].css({position:'fixed', top:'10px', maxHeight:d});
+                uls[0].css({position:'fixed', top:'10px', maxHeight:d, width:toc.width()+"px"});
             } else {
                 uls[0].attr("style","");
             }
